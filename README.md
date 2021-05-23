c205bcfa7586e6561491e04e9e00afe114338c00
Peter Kaagman <prjv.kaagman@gmail.com>
2021-05-23 14:59:50 +0000

    cleaned up README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # mysql_tutor
 NodeJS, Express, Sequelize, MySQL tutorial
+
+## Config files
+Files in config are not in the repository
+
+### config/db.config.js
+
+```javascript
+module.exports = {
+  HOST: "localhost",
+  USER: "user",
+  PASSWORD: "password",
+  DB: "workout",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+```
+
--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://192.168.5.4:3001"
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Simple route
 // Bij MDN deden ze dit anders
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
   res.json({message: "Welcome to my tutorial"});
 });
 
